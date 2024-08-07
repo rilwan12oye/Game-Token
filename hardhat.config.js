b2d992c1384f0811d7acdad81cd09dd2e11a85d0
Rilwan Oyewole <rilwan12.oyewole@gmail.com>
2024-08-07 18:48:38 +0000

    project completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,31 +7,31 @@ const { URL, PRIVATE_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
-  // defaultNetwork: "fuji",
-  // etherscan: {
-  //   apiKey: {
-  //     snowtrace: "snowtrace", // apiKey is not required, just set a placeholder
-  //   },
-  //   customChains: [
-  //     {
-  //       network: "snowtrace",
-  //       chainId: 43113,
-  //       urls: {
-  //         apiURL:
-  //           "https://api.routescan.io/v2/network/testnet/evm/43113/etherscan",
-  //         browserURL: "https://testnet.snowtrace.io",
-  //       },
-  //     },
-  //   ],
-  // },
-  // networks: {
-  //   snowtrace: {
-  //     url: "https://api.avax-test.network/ext/bc/C/rpc",
-  //     accounts: [`0x${PRIVATE_KEY}`],
-  //   },
-  //   fuji: {
-  //     url: URL,
-  //     accounts: [`0x${PRIVATE_KEY}`],
-  //   },
-  // },
+  defaultNetwork: "fuji",
+  etherscan: {
+    apiKey: {
+      snowtrace: "snowtrace", // apiKey is not required, just set a placeholder
+    },
+    customChains: [
+      {
+        network: "snowtrace",
+        chainId: 43113,
+        urls: {
+          apiURL:
+            "https://api.routescan.io/v2/network/testnet/evm/43113/etherscan",
+          browserURL: "https://testnet.snowtrace.io",
+        },
+      },
+    ],
+  },
+  networks: {
+    snowtrace: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    fuji: {
+      url: URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
